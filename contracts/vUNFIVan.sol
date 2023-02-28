96bf8359cc2346023217f4118a07e7297d653f9d
A Fistful of Stray Cat Hair <adam@unifiprotocol.com>
2023-02-27 20:16:24 -0500

    feat: remove inheritance, combine in token contract

--- contracts/vUNFIVan.sol
+++ contracts/vUNFIVan.sol
@@ -2,102 +2,181 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./Staking.sol";
 
 contract UnifiProtocolVotingToken is
-  ERC20,
-  ERC20Burnable,
-  Pausable,
-  Ownable,
-  ERC20Permit,
-  ERC20Votes,
-  Staking,
-  ReentrancyGuard
+    ERC20,
+    ERC20Burnable,
+    Pausable,
+    Ownable,
+    ERC20Permit,
+    ERC20Votes,
+    ReentrancyGuard
 {
-  constructor(
-    address unfiTokenAddress
-  )
-    ERC20("Unifi Protocol Voting Token", "vUNFI")
-    ERC20Permit("Unifi Protocol Voting Token")
-    Staking(unfiTokenAddress)
-  {}
+    uint256 public duration;
+    uint256 public finishAt;
+    uint256 public updatedAt;
+    uint256 public rewardRate;
+    uint256 public rewardPerTokenStored;
+    uint256 public totalStaked;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+    mapping(address => uint256) public amountUserStaked;
 
-  function pause() public onlyOwner {
-    _pause();
-  }
+    IERC20 public immutable unfiToken;
 
-  function unpause() public onlyOwner {
-    _unpause();
-  }
-
-  function mint(address to, uint256 amount) public onlyOwner {
-    _mint(to, amount);
-  }
-
-  function _beforeTokenTransfer(
-    address from,
-    address to,
-    uint256 amount
-  ) internal override whenNotPaused {
-    super._beforeTokenTransfer(from, to, amount);
-  }
-
-  function stake(uint256 amount) public updateReward(msg.sender) moreThanZero(amount) {
-    // keep track of how much this user has staked
-    // keep track of how much token we have total
-    // transfer the tokens to this contract
-    /** @notice Be mindful of reentrancy attack here */
-    //emit event
-    s_unfiAddress.approve(address(this), amount);
-    bool success = s_unfiAddress.transferFrom(msg.sender, address(this), amount);
-    if (!success) {
-      revert Staking__TransferFailed();
+    constructor(address unfiTokenAddress)
+        ERC20("Unifi Protocol Voting Token", "vUNFI")
+        ERC20Permit("Unifi Protocol Voting Token")
+    {
+        unfiToken = IERC20(unfiTokenAddress);
     }
-    _mint(msg.sender, amount);
-    s_balances[msg.sender] += amount;
-    s_totalSupply += amount;
-  }
 
-  function withdraw(uint256 amount) public updateReward(msg.sender) moreThanZero(amount) {
-    bool balanceCheck = s_balances[msg.sender] >= amount;
-    if (!balanceCheck) {
-      revert Withdraw__TransferFailed();
+    modifier updateReward(address _account) {
+        rewardPerTokenStored = rewardPerToken();
+        updatedAt = lastTimeRewardApplicable();
+        if (_account != address(0)) {
+            rewards[_account] = earned(_account);
+            userRewardPerTokenPaid[_account] = rewardPerTokenStored;
+        }
+        _;
     }
-    claimReward();
-    s_balances[msg.sender] -= amount;
-    s_totalSupply -= amount;
-    bool success = s_unfiAddress.transfer(msg.sender, amount);
-    if (!success) {
-      revert Withdraw__TransferFailed();
+
+    function pause() public onlyOwner {
+        _pause();
     }
-    _burn(msg.sender, amount);
-  }
 
-  function updateRewardRate(uint256 _rewardRate) public onlyOwner updateReward(msg.sender) {
-    rewardRate = _rewardRate;
-  }
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
-  // The following functions are overrides required by Solidity.
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
-  function _afterTokenTransfer(
-    address from,
-    address to,
-    uint256 amount
-  ) internal override(ERC20, ERC20Votes) {
-    super._afterTokenTransfer(from, to, amount);
-  }
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 
-  function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
-    super._mint(to, amount);
-  }
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return _min(finishAt, block.timestamp);
+    }
 
-  function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
-    super._burn(account, amount);
-  }
+    function rewardPerToken() public view returns (uint256) {
+        if (totalStaked == 0) {
+            return rewardPerTokenStored;
+        }
+
+        return
+            rewardPerTokenStored +
+            (rewardRate * (lastTimeRewardApplicable() - updatedAt) * 1e18) /
+            totalStaked;
+    }
+
+    function stake(uint256 _amount)
+        external
+        updateReward(msg.sender)
+        nonReentrant
+    {
+        require(_amount > 0, "vUNFI: NO_UNFI_TO_STAKE");
+        unfiToken.transferFrom(msg.sender, address(this), _amount);
+        unfiToken.transferFrom(msg.sender, address(this), _amount);
+        _mint(msg.sender, _amount);
+        amountUserStaked[msg.sender] += _amount;
+        totalStaked += _amount;
+    }
+
+    function withdraw(uint256 _amount)
+        external
+        updateReward(msg.sender)
+        nonReentrant
+    {
+        require(_amount > 0, "vUNFI: CANNOT_UNSTAKE_ZERO");
+        getReward();
+        _burn(msg.sender, _amount);
+        amountUserStaked[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        unfiToken.transfer(msg.sender, _amount);
+    }
+
+    function earned(address _account) public view returns (uint256) {
+        return
+            ((amountUserStaked[_account] *
+                (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
+            rewards[_account];
+    }
+
+    function getReward() public updateReward(msg.sender) nonReentrant {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            unfiToken.transfer(msg.sender, reward);
+        }
+    }
+
+    function setRewardsDuration(uint256 _duration) external onlyOwner {
+        duration = _duration;
+    }
+
+    function notifyRewardAmount(uint256 _amount)
+        external
+        onlyOwner
+        updateReward(address(0))
+    {
+        if (block.timestamp >= finishAt) {
+            rewardRate = _amount / duration;
+        } else {
+            uint256 remainingRewards = (finishAt - block.timestamp) *
+                rewardRate;
+            rewardRate = (_amount + remainingRewards) / duration;
+        }
+        finishAt = block.timestamp + duration;
+        updatedAt = block.timestamp;
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+
+    function updateRewardRate(uint256 _rewardRate)
+        public
+        onlyOwner
+        updateReward(msg.sender)
+    {
+        rewardRate = _rewardRate;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
 }
