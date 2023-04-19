bb14040e7d4d5b2d291cc9b18c4e1b6b0e046eee
Daniel Blanco Parla <dblancoparla@gmail.com>
2023-04-19 11:30:28 +0200

    feat: vUNIFI.sol clean up

--- contracts/vUNIFI.sol
+++ contracts/vUNIFI.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
